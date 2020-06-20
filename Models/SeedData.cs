35b33012df9aa20b19895dd67f74839e0cb753e6
PC <PC@BARBARA-11>
2020-06-20 14:40:18 -0600

    Some stuff

---
+++ Models/SeedData.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace MvcMovie.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new MvcMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<MvcMovieContext>>()))
+            {
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Movie
+                    {
+                        Title = "The Other Side of Heaven",
+                        ReleaseDate = DateTime.Parse("2001-12-14"),
+                        Genre = "Romantic Comedy",
+                        Rating = "PG",
+                        Price = 7.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Testaments: Of One Fold and One Shepherd",
+                        ReleaseDate = DateTime.Parse("2000-3-24"),
+                        Genre = "Religious",
+                        Rating = "G",
+                        Price = 4.99M
+                    },
+
+                    new Movie
+                    {
+                        Title = "The Best Two Years",
+                        ReleaseDate = DateTime.Parse("2004-2-20"),
+                        Genre = "Comedy",
+                        Rating = "PG",
+                        Price = 9.99M
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
